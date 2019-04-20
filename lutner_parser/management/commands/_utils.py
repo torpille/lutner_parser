ac1c6826bf5844395b0b488dea85d07e58fcc4dd
torpille <torpille113@gmail.com>
2019-04-20 08:17:05 +0300

    'init'

--- lutner_parser/management/commands/_utils.py
+++ lutner_parser/management/commands/_utils.py
@@ -1,11 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 from lutner_parser.models import Product, Section, Category, Brandname
 
 
-def get_html(url):
-    r = requests.get(url, timeout = (1000, 1000))
-    return r.text
+def get_soup(url):
+
+    for i in range(10):
+        try:
+            html = requests.get(url, timeout = (100, 100)).text
+            soup = BeautifulSoup(html, 'html5lib')
+            break
+        except requests.exceptions.ConnectionError:
+            print('----waiting----')
+            time.sleep(1)
+    if soup:
+        return soup
+    else:
+        return
 
 def get_or_none(classmodel, **kwargs):
     try:
@@ -15,59 +27,48 @@ def get_or_none(classmodel, **kwargs):
     except classmodel.MultipleObjectsReturned:
         return classmodel.objects.filter(**kwargs).first()
 
-def create_product(url, unparsed_links):
-    for i in range(10):
-        try:
-            html = get_html(url)
-            break
-        except requests.exceptions.ConnectionError:
-            print('----waiting----')
-            time.sleep(1)
+def create_product(url):
+    
         
-    if html:
-        soup = BeautifulSoup(html, 'html5lib')
+    soup = get_soup(url)
+    if soup:
         product = get_or_none(Product, link=url)
         if product:
             return
         else:
             product = Product(link=url)
-            try:
-                product.name = soup.find('h1').text
-                product.article = product.name.split(' ')[0]
-                section_category = soup.find(class_ = 'breadcrumb').text.split('>')
-                section_text = section_category[-2].strip()
-                category_text = section_category[-1].strip()
-                section = get_or_none(Section, name=section_text)
-                if not section:
-                    section = Section(name=section_text)
-                    section.save()
-                category = get_or_none(Category, name=category_text)
-                if not category:
-                    category = Category(name=category_text, section=section)
-                    category.save()
-                product.category=category
-                brandname_text = soup.find(class_='product-features-table').find_all('tr')[0].find_all('td')[1].text
-                brandname = get_or_none(Brandname, name=brandname_text)
-                if not brandname:
-                    brandname = Brandname(name=brandname_text)
-                    brandname.save()
-                product.brandname = brandname
-                product.save()
-                print(product.name)
-            except AttributeError as error:
-                unparsed_links.append(url)
+            product.name = soup.find('h1').text
+            product.article = product.name.split(' ')[0]
+            section_category = soup.find(class_ = 'breadcrumb').text.split('>')
+            section_text = section_category[-2].strip()
+            category_text = section_category[-1].strip()
+            section = get_or_none(Section, name=section_text)
+            if not section:
+                section = Section(name=section_text)
+                section.save()
+            category = get_or_none(Category, name=category_text)
+            if not category:
+                category = Category(name=category_text, section=section)
+                category.save()
+            product.category=category
+            brandname_text = soup.find(class_='product-features-table').find_all('tr')[0].find_all('td')[1].text
+            brandname = get_or_none(Brandname, name=brandname_text)
+            if not brandname:
+                brandname = Brandname(name=brandname_text)
+                brandname.save()
+            product.brandname = brandname
+            product.save()
+            print(product.name)
+        
 
 
 
 
 
 
-
-def find_category_links(page):
+def find_category_links(url):
     category_links = []
-    URL = page
-    current_page = requests.get(URL)
-    soup = BeautifulSoup(current_page.content, 'html.parser')
+    soup = get_soup(url)
     for a in  soup.find_all( class_ = 'second'):
         for href in a.find_all('a', href=True):
             category_links.append( href['href'])
@@ -77,10 +78,9 @@ def find_category_links(page):
     
 def find_page_links(link):
     page_links = set()
-    URL = 'https://lutner.ru' + link
-    page_links.add(URL + '?PAGEN_1=1')
-    current_page = requests.get(URL)
-    soup = BeautifulSoup(current_page.content, 'html.parser')
+    url = 'https://lutner.ru' + link
+    page_links.add(url + '?PAGEN_1=1')
+    soup = get_soup(url)
     pages = soup.find_all( class_='pl')
     for page in pages:
         elems = str(page).split('"')
@@ -93,8 +93,7 @@ def find_page_links(link):
 
 def find_item_links(url):
     item_links = set()
-    current_page = requests.get(url)
-    soup = BeautifulSoup(current_page.content, 'html.parser')
+    soup = get_soup(url)
     items = soup.find_all( class_='product-item-title')
     for item in items:
         href = item.find('a', href=True).get('href')
--- lutner_parser/management/commands/parser_init.py
+++ lutner_parser/management/commands/parser_init.py
@@ -9,7 +9,6 @@ class Command(BaseCommand):
         category_links = find_category_links('https://lutner.ru/catalog/')
         page_links = set()
         item_links = set()
-        unparsed_links = []
         for link in category_links:
             page_links.update(find_page_links(link))
             print('cl', link)
@@ -19,7 +18,5 @@ class Command(BaseCommand):
         # item_links = ['https://lutner.ru/catalog/blok_fleyty_aksessuary/1094_prima_blokfleyta_soprano_bezhevyy_plastik_derevo_barochnaya_sistema_mollenhauer/']
         for url in item_links:
             print(url)
-            create_product(url, unparsed_links)
-        for url in unparsed_links:
-            print('try', url)
-            create_product(url, unparsed_links)
+            create_product(url)
+
